7df966b5b487b3a7d3522195f80c303d83c938ce
Orlando <peraza.oj@gmail.com>
2025-04-04 17:28:18 +1300

    Add LastName property in Student class and a new method for deleting a Student

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,7 @@
 namespace StudentManager.Models{
 public class Student{
     public string Name { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
     public int Age { get; set; } 
 }
 
--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
-﻿using StudentManager.Models;
+﻿using System.Threading.Tasks;
+using StudentManager.Models;
+using StudentManager.Services;
 
-List<Student> students = new List<Student>();
+StudentService service = new StudentService();
+List<Student> students = await service.LoadStudentsAsync();
+
 bool running = true;
 while (running){
     Console.WriteLine("\n--- Student Manager ---");
             Console.WriteLine("1. Add Student");
             Console.WriteLine("2. List Students");
             Console.WriteLine("3. Search Student");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("4. Delete Student");
+            Console.WriteLine("5. Exit");
             Console.Write("Choose an option: ");
 
             string input = Console.ReadLine();
@@ -15,7 +20,7 @@ while (running){
             switch (input)
             {
                 case "1":
-                    AddStudent();
+                    await AddStudent();
                     break;
                 case "2":
                     ListStudents();
@@ -24,6 +29,9 @@ while (running){
                     SearchStudent();
                     break;
                 case "4":
+                    await DeleteStudent();
+                    break;
+                case "5":
                     running = false;
                     break;
                 default:
@@ -32,16 +40,21 @@ while (running){
             }
 }
 
-void AddStudent()
+async Task AddStudent()
     {
         Console.Write("Enter student name: ");
         string name = Console.ReadLine();
 
+        Console.Write("Enter student last name: ");
+        string lastName = Console.ReadLine();
+
         Console.Write("Enter student age: ");
         int age = Convert.ToInt32(Console.ReadLine());
 
-        Student newStudent = new Student { Name = name, Age = age };
+        Student newStudent = new Student { Name = name, LastName = lastName, Age = age };
         students.Add(newStudent);
+
+        await service.SaveStudentsAsync(students);
 
         Console.WriteLine("Student added!");
     }
@@ -51,20 +64,60 @@ void ListStudents()
         Console.WriteLine("\n--- All Students ---");
         foreach (var student in students)
         {
-            Console.WriteLine($"Name: {student.Name}, Age: {student.Age}");
+            Console.WriteLine($"Name: {student.Name} {student.LastName}, Age: {student.Age}");
         }
     }
 
 void SearchStudent()
     {
         Console.Write("Enter name to search: ");
-        string search = Console.ReadLine();
+        string name = Console.ReadLine();
 
-        var found = students.Find(s => s.Name.Equals(search, StringComparison.OrdinalIgnoreCase));
+        Console.Write("Enter last name to search: ");
+        string lastName = Console.ReadLine();
+
+
+        var found = students.Find(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase) && s.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
 
         if (found != null)
         {
-            Console.WriteLine($"Found: {found.Name}, Age: {found.Age}");
+            Console.WriteLine($"Found: {found.Name} {found.LastName}, Age: {found.Age}");
+        }
+        else
+        {
+            Console.WriteLine("Student not found.");
+        }
+    }
+
+    async Task DeleteStudent()
+    {
+        if (students.Count == 0)
+    {
+        Console.WriteLine("No students to delete.");
+        return;
+    }
+        Console.Write("Enter name to delete: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter last name to search: ");
+        string lastName = Console.ReadLine();
+
+        var found = students.Find(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase) && s.LastName.Equals(lastName, StringComparison.OrdinalIgnoreCase));
+
+        if (found != null)
+        {
+            Console.Write($"Are you sure you want to delete de student: {found.Name} {found.LastName} ? (y/n):");
+            string confirmation = Console.ReadLine()?.Trim().ToLower();
+            if(confirmation == "y"){
+                students.Remove(found);
+                await service.SaveStudentsAsync(students);
+                Console.WriteLine("Student deleted!");
+            }
+            else{
+                Console.WriteLine("Deletion canceled.");
+            }
+
+            
         }
         else
         {
